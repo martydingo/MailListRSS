34498630075bce220921baef8eb747a221ea23f5
Martin George <marty@dingo.foo>
2024-08-08 12:45:08 +0100

    commit

--- MailListRSS/__init__.py
+++ MailListRSS/__init__.py
@@ -1,4 +1,5 @@
 from office365.graph_client import GraphClient
+from pprint import pprint
 
 
 class MailListRSS:
@@ -18,10 +19,29 @@ class MailListRSS:
         )
 
     def __poll_office365__(self):
+        # messages = (
+        #     self.client.users[self.configuration["inbox"]]
+        #     .messages.get()
+        #     # .filter("ccRecipients/any(i:i ')")
+        #     .filter("startswith(subject, '[DNSOP]')")
+        #     .top(1)
+        #     .execute_query()
+        # )
+        # messages = (
+        #     self.client.users[self.configuration["inbox"]]
+        #     .mail_folders[
+        #         "AAMkADcxYzY4ODFkLTY1MDYtNGMyNi04MmNmLTg4YTU0Mjg3OGJmYgAuAAAAAABiSP8jCFYLSZrBWYpeHWpYAQBbd8M1_CGsQ63mmDqKE8XTAAPDml8ZAAA="
+        #     ]
+        #     .child_folders.get_all()
+        #     .execute_query()
+        # )
         messages = (
             self.client.users[self.configuration["inbox"]]
+            .mail_folders[
+                "AAMkADcxYzY4ODFkLTY1MDYtNGMyNi04MmNmLTg4YTU0Mjg3OGJmYgAuAAAAAABiSP8jCFYLSZrBWYpeHWpYAQBbd8M1_CGsQ63mmDqKE8XTAAPDml8aAAA="
+            ]
             .messages.get()
-            .top(10)
             .execute_query()
         )
-        print(messages)
+
+        print(messages.to_json())
