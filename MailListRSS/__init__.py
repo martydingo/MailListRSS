b4b1b9d89c95d2c5e85e145c332f1e7f2dc8164e
Martin George <marty@dingopaws.net>
2024-08-08 14:00:56 +0100

    commit

--- MailListRSS/__init__.py
+++ MailListRSS/__init__.py
@@ -1,5 +1,8 @@
 from office365.graph_client import GraphClient
 from pprint import pprint
+from feedgen.feed import FeedGenerator
+import datetime
+import pytz
 
 
 class MailListRSS:
@@ -25,8 +28,9 @@ class MailListRSS:
             .messages.get()
             .execute_query()
         )
-
+        rss_feed = FeedGenerator()
         for message in messages:
+            rss_entry = rss_feed.add_entry()
             try:
                 mailingListSubjectList = message.subject.split("[")[1].split("]")
                 mailingListTopic = mailingListSubjectList[0]
@@ -37,12 +41,49 @@ class MailListRSS:
                 mailingListTopic = "Unknown"
                 mailingListSubject = message.subject
 
-            id = message.webLink
-            title = mailingListSubject
-            subtitle = mailingListTopic
-            auther = {
+            rss_entry.id(message.web_link)
+            rss_entry.link({"href": message.web_link})
+            rss_entry.title(mailingListSubject)
+            rss_entry.pubDate(
+                message.created_datetime.replace(tzinfo=datetime.timezone.utc)
+            )
+            rss_entry.content(str(message.body))
+            rss_entry.category({"term": mailingListTopic})
+            rss_entry.author = {
                 "name": message.sender.emailAddress.name,
                 "email": message.sender.emailAddress.address,
             }
+            rss_entry.summary = str(message.body_preview)
+
+            # contributors = []
+            # contributors.append(
+            #     {
+            #         "name": message.sender.emailAddress.name,
+            #         "email": message.sender.emailAddress.address,
+            #     }
+            # )
+            # for contributor in message.to_recipients:
+            #     contributors.append(
+            #         {
+            #             "name": contributor.emailAddress.name,
+            #             "email": contributor.emailAddress.address,
+            #         }
+            #     )
+            # for contributor in message.cc_recipients:
+            #     contributors.append(
+            #         {
+            #             "name": contributor.emailAddress.name,
+            #             "email": contributor.emailAddress.address,
+            #         }
+            #     )
+            # rss_entry.contributors = contributors
+            # print(rss_entry.__dict__)
             # print(mailingListTopic)
             # print(mailingListSubject)
+            # print(contributors)
+        rss_feed.title("Mailing Lists")
+        rss_feed.description("...")
+        rss_feed.link({"href": "dingo.foo"})
+        rssfeed = rss_feed.rss_str(pretty=True)
+        print(rssfeed)
+        rssfeed_file = rss_feed.rss_file("mailing-list.rss")
